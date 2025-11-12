70ea5748c7c26c0fdff76659b57df9d090751e79
Ishaaq Roosevelt <80652943+KTechDev4213@users.noreply.github.com>
2025-11-11 21:52:02 -0500

    Add project files.

---
+++ craigline/Program.cs
@@ -0,0 +1,10 @@
+﻿using craigline;
+using Spectre.Console.Cli;
+var app = new CommandApp();
+app.Configure(config =>
+{
+    config.SetApplicationName("craigline");
+    config.AddCommand<SearchCommand>("search")
+        .WithDescription("Search Craigline.");
+});
+app.Run(args);
---
+++ craigline/SearchCommand.cs
@@ -0,0 +1,24 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace craigline
+{
+    internal class SearchCommand:Command<SearchCommand.Settings>
+    {
+        public class Settings : CommandSettings
+        {
+            [CommandArgument(0, "[term]")]
+            public string Term { get; set; }
+        }
+        public override int Execute(CommandContext context, Settings settings, CancellationToken cancellationToken)
+        {
+            AnsiConsole.MarkupLine($"Hello, [blue]{settings.Term}[/]");
+            return 0;
+        }
+    }
+}
